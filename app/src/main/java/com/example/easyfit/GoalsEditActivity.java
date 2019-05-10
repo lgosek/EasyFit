db57351d571231371bdac44afd43e98f5f3e2ebf
lgosek <luki2012@gmail.com>
2019-05-10 17:33:04 +0200

    Defining nutrition goals added

--- app/src/main/java/com/example/easyfit/GoalsEditActivity.java
+++ app/src/main/java/com/example/easyfit/GoalsEditActivity.java
@@ -1,12 +1,35 @@
 package com.example.easyfit;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.easyfit.notifications.NotificationManager;
 
 public class GoalsEditActivity extends AppCompatActivity {
+
+    NumberPicker carbsPicker, fatPicker, proteinsPicker;
+    EditText caloriesGoalEdit;
+    TextView warningText, carbsGrams, protGrams, fatGrams;
+
+    int FATCALS = 9;
+    int CARBSCALS = 4;
+    int PROTCALS = 4;
+    int caloriesGoalInt;
+
+    boolean error = false;
+
+    private final String sharedPreferencesFileName = "com.example.easyfit.sharedpreferences";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +38,145 @@ public class GoalsEditActivity extends AppCompatActivity {
 
 //        Toolbar toolbar = findViewById(R.id.toolbar2);
 //        setSupportActionBar(toolbar);
+
+
+        warningText = findViewById(R.id.goalsEditWarningText);
+        warningText.setText("");
+
+        caloriesGoalEdit = findViewById(R.id.caloriesEdit);
+
+        carbsPicker = findViewById(R.id.carbsPicker);
+        fatPicker = findViewById(R.id.fatPicker);
+        proteinsPicker = findViewById(R.id.proteinsPicker);
+
+        carbsGrams = findViewById(R.id.goalsCarbsAmount);
+        protGrams = findViewById(R.id.goalsProteinsAmount);
+        fatGrams = findViewById(R.id.goalsFatAmount);
+
+
+        SharedPreferences sh = getSharedPreferences(this.sharedPreferencesFileName, MODE_PRIVATE);
+
+        String calGoal = sh.getString("caloriesGoal", "2000");
+        caloriesGoalEdit.setText(calGoal);
+        caloriesGoalInt = Integer.parseInt(calGoal);
+        int carbsPercent = sh.getInt("carbsGoal", 50);
+        int protPercent = sh.getInt("proteinsGoal", 20);
+        int fatPercent = sh.getInt("fatGoal", 30);
+
+        updateGrams(protPercent,carbsPercent,fatPercent);
+
+
+        //String[] values = {"0","5","10","15","20","25","30","35","40","45","50","55","60","65","70","75","80","85","90","95","100"};
+
+//        carbsPicker.setDisplayedValues(values);
+//        fatPicker.setDisplayedValues(values);
+//        proteinsPicker.setDisplayedValues(values);
+        carbsPicker.setMinValue(0);
+        carbsPicker.setMaxValue(100);
+        carbsPicker.setValue(carbsPercent);
+
+        fatPicker.setMinValue(0);
+        fatPicker.setMaxValue(100);
+        fatPicker.setValue(fatPercent);
+
+        proteinsPicker.setMinValue(0);
+        proteinsPicker.setMaxValue(100);
+        proteinsPicker.setValue(protPercent);
+
+        caloriesGoalEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String text = caloriesGoalEdit.getText().toString();
+                if(!text.equals("")) {
+                    caloriesGoalInt = Integer.parseInt(text);
+                    error = false;
+                }
+                else {
+                    caloriesGoalInt = 0;
+                    error = true;
+                }
+                updateGrams(proteinsPicker.getValue(), carbsPicker.getValue(), fatPicker.getValue());
+            }
+        });
+
+        carbsPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                if(newVal + fatPicker.getValue() + proteinsPicker.getValue() != 100) {
+                    warningText.setText(getResources().getText(R.string.warningText));
+                    error = true;
+                }
+                else {
+                    warningText.setText("");
+                    error = false;
+                }
+
+                int carbs = Math.round((float)(newVal * caloriesGoalInt) / 100 / CARBSCALS);
+                carbsGrams.setText(carbs+"g");
+            }
+        });
+
+        fatPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                if(newVal + carbsPicker.getValue() + proteinsPicker.getValue() != 100) {
+                    warningText.setText(getResources().getText(R.string.warningText));
+                    error = true;
+                }
+                else {
+                    warningText.setText("");
+                    error = false;
+                }
+
+                int fat = Math.round((float)(newVal * caloriesGoalInt) / 100 / FATCALS);
+                fatGrams.setText(fat+"g");
+            }
+        });
+
+        proteinsPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                if(newVal + fatPicker.getValue() + carbsPicker.getValue() != 100) {
+                    warningText.setText(getResources().getText(R.string.warningText));
+                    error = true;
+                }
+                else {
+                    warningText.setText("");
+                    error = false;
+                }
+
+                int prot = Math.round((float)(newVal * caloriesGoalInt) / 100 / PROTCALS);
+                protGrams.setText(prot+"g");
+            }
+        });
+
+    }
+
+    private void updateGrams(int protPercent, int carbsPercent, int fatPercent) {
+        if(caloriesGoalInt==0){
+            carbsGrams.setText("-");
+            protGrams.setText("-");
+            fatGrams.setText("-");
+            return;
+        }
+
+        int prot = Math.round((float)(protPercent * caloriesGoalInt) / 100 /PROTCALS);
+        int carbs = Math.round((float)(carbsPercent * caloriesGoalInt) / 100 / CARBSCALS);
+        int fat = Math.round((float)(fatPercent * caloriesGoalInt) / 100 / FATCALS);
+
+        carbsGrams.setText(carbs+"g");
+        protGrams.setText(prot+"g");
+        fatGrams.setText(fat+"g");
     }
 
     @Override
@@ -27,6 +189,21 @@ public class GoalsEditActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.goalsEditAccept:
+
+                if(error){
+                    Toast.makeText(this, "Wprowadź poprawne dane!", Toast.LENGTH_LONG).show();
+                    break;
+                }
+
+                //saving data to shared preferences file
+                SharedPreferences sh = getSharedPreferences(this.sharedPreferencesFileName, MODE_PRIVATE);
+                SharedPreferences.Editor editor = sh.edit();
+                editor.putString("caloriesGoal", caloriesGoalEdit.getText().toString());
+                editor.putInt("proteinsGoal", proteinsPicker.getValue());
+                editor.putInt("carbsGoal", carbsPicker.getValue());
+                editor.putInt("fatGoal", fatPicker.getValue());
+                editor.apply();
+
                 finish();
                 break;
         }
--- app/src/main/java/com/example/easyfit/GoalsFragment.java
+++ app/src/main/java/com/example/easyfit/GoalsFragment.java
@@ -1,6 +1,7 @@
 package com.example.easyfit;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,11 +10,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class GoalsFragment extends Fragment {
 
     View view;
     Button editButton;
+
+    int FATCALS = 9;
+    int CARBSCALS = 4;
+    int PROTCALS = 4;
+
+    TextView goalsCaloriesValueText, goalsCarbsValueText, goalsProteinsValueText, goalsFatValueText;
+
+    private final String sharedPreferencesFileName = "com.example.easyfit.sharedpreferences";
+
 
     @Nullable
     @Override
@@ -22,8 +35,61 @@ public class GoalsFragment extends Fragment {
         editButton = view.findViewById(R.id.goalsEditButton);
         setEditOnClick();
 
+        goalsCaloriesValueText = view.findViewById(R.id.goalsCaloriesValue);
+        goalsCarbsValueText = view.findViewById(R.id.goalsCarbsValue);
+        goalsProteinsValueText = view.findViewById(R.id.goalsProteinsValue);
+        goalsFatValueText = view.findViewById(R.id.goalsFatValue);
 
+        //reading values from shared preferences file and setting up values
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        SharedPreferences sh = getActivity().getSharedPreferences(this.sharedPreferencesFileName, MODE_PRIVATE);
+
+        String calGoal = sh.getString("caloriesGoal", "-");
+        int carbsPercent = sh.getInt("carbsGoal", -1);
+        int protPercent = sh.getInt("proteinsGoal", -1);
+        int fatPercent = sh.getInt("fatGoal", -1);
+
+        setValues(calGoal, carbsPercent, protPercent, fatPercent);
+    }
+
+    private void setValues(String calGoal, int carbsPercent, int protPercent, int fatPercent) {
+        goalsCaloriesValueText.setText(calGoal);
+        int caloriesGoalInt = 0;
+        if(!calGoal.equals("-"))
+            caloriesGoalInt = Integer.parseInt(calGoal);
+        if(carbsPercent != -1){
+            if(caloriesGoalInt != 0){
+                int carbs = Math.round((float)(carbsPercent * caloriesGoalInt) / 100 / CARBSCALS);
+                String text = new StringBuilder().append("(").append(carbs).append("g) ").append(carbsPercent).append("%").toString();
+                goalsCarbsValueText.setText(text);
+            }
+        }
+        else
+            goalsCarbsValueText.setText("-");
+        if(protPercent != -1){
+            if(caloriesGoalInt != 0){
+                int prots = Math.round((float)(protPercent * caloriesGoalInt) / 100 / PROTCALS);
+                String text = new StringBuilder().append("(").append(prots).append("g) ").append(protPercent).append("%").toString();
+                goalsProteinsValueText.setText(text);
+            }
+        }
+        else
+            goalsProteinsValueText.setText("-");
+        if(fatPercent != -1){
+            if(caloriesGoalInt != 0){
+                int fat = Math.round((float)(fatPercent * caloriesGoalInt) / 100 / FATCALS);
+                String text = new StringBuilder().append("(").append(fat).append("g) ").append(fatPercent).append("%").toString();
+                goalsFatValueText.setText(text);
+            }
+        }
+        else
+            goalsFatValueText.setText("-");
     }
 
     private void setEditOnClick(){
